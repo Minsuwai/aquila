d338093211accbdad4cd63a892b83ef2897404b4
minsuwai <mswwai15@gmail.com>
2024-01-12 19:03:07 +0630

    first commit

---
+++ wp-content/themes/aquila/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main Template file.
+ * 
+ * @package Aquila
+ * 
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WordPress Theme</title>
+</head>
+<body>
+    Hello wordPress
+</body>
+</html>
